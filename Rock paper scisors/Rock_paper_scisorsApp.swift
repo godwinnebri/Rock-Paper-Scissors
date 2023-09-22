2289f9ef7792216cdf84fb422e10201807da1ebc
Godwin Ebri <104990430+godwinnebri@users.noreply.github.com>
2023-09-22 16:11:36 +0100

    Initial Commit

---
+++ Rock paper scisors/Rock_paper_scisorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_paper_scisorsApp.swift
+//  Rock paper scisors
+//
+//  Created by Godwin IE on 22/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct Rock_paper_scisorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
